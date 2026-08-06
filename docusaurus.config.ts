b45f6da9353a06f3e08237d726f4f58271a79046
Dennis <git@dennispietsch.com>
2026-08-06 15:18:56 +0200

    Improve sitemap and 404 template

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -45,6 +45,10 @@ const config: Config = {
           sidebarPath: './sidebars.ts',
           breadcrumbs: false,
         },
+        sitemap: {
+          changefreq: null,
+          priority: null,
+        },
         blog: false,
         theme: {
           customCss: './src/css/custom.css',
